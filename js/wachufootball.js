288e412cebf2606f924a63a6be741e298cc5cadf
Drew C <drew.ciaramitaro51@gmail.com>
2017-12-08 07:38:14 -0500

    lol

--- js/wachufootball.js
+++ js/wachufootball.js
@@ -9,3 +9,36 @@ $(document).on('scroll', function (e) {
     }
 });
 
+function doneLoading() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("content").style.display = "block";
+}
+
+
+// When the page is fully loaded, it calls the load() function, which hides the loading icon and shows the rest of the content
+document.onreadystatechange = function () {
+    if (document.readyState == 'complete') { doneLoading(); }
+}
+
+// This calls the page transitions
+$(document).ready(function () {
+    $(".animsition").animsition({
+        inClass: 'zoom-in-sm',
+        outClass: 'zoom-out-sm',
+        inDuration: 200,
+        outDuration: 200,
+        linkElement: 'a:not([target="_blank"]):not([href^="#"])',
+        loading: false,
+        loadingParentElement: 'body',
+        loadingClass: 'animsition-loading',
+        loadingInner: '',
+        timeout: true,
+        timeoutCountdown: 10,
+        onLoadEvent: true,
+        browser: ['animation-duration', '-webkit-animation-duration'],
+        overlay: false,
+        overlayClass: 'animsition-overlay-slide',
+        overlayParentElement: 'body',
+        transition: function (url) { window.location.href = url; }
+    });
+});
